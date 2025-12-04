f620d45a971233bc11b5e16f96dd089c11ba7268
Muzzamil Yasin <muzzamilyasin28@gmail.com>
2025-12-04 12:09:02 +0500

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    base: '/Lumina-Store/',
     plugins: [react()],
     define: {
-      // This bridges the gap so process.env.API_KEY works locally
       'process.env.API_KEY': JSON.stringify(env.API_KEY || '')
     }
   };
