20c4b4c51e6f6ac91562298673e3b5cc4d91a130
Muzzamil Yasin <muzzamilyasin28@gmail.com>
2025-12-05 11:27:20 +0500

    Fixing paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: './',  
+    base: '/Lumina-Store/',  
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY || '')
